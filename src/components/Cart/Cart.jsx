4d5d4befa22112fcf2845e2a07a6dc63ab6b0796
saifu9421 <saifursaif905@gmail.com>
2023-09-15 05:07:15 +0600

    added a card

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,31 @@
 
 
-const Cart = () => {
+const Cart = ({seletedActors,remaining,totalCost}) => {
+          
+    console.log(seletedActors);
+          
     return (
 
         <div className="text-right mt-20 mr-28  ">
-     <h1 className="text-3xl">This is card</h1>
+     <h1 className="text-3xl text-yellow-200">This is card</h1>
+
+     <h3 className="text-yellow-100">total actors:{seletedActors.length}</h3>
+     <h5 className="text-yellow-200">
+     remaining:{remaining}
+     </h5>
+
+     <h5 className="text-yellow-100">
+        Totalcoust:{totalCost}
+     </h5>
+
+     {
+        seletedActors.map(actor=> (
+            <li key={actor.id} className="text-yellow-100">
+                {actor.name}
+            </li>
+        ))
+     }
+     
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,45 @@
 /* eslint-disable react/jsx-key */
-
+import './Home.css';
 import { useEffect, useState } from "react";
 import Cart from "../Cart/Cart";
 const Home = () => {
      
      const[allActors,setAllActors] = useState([]);
-
+        const [seletedActors,setSeletedActors] = useState([]);
+        const [remaining , setReamining] =  useState(0);
+      const [totalCost ,setTotalCost ] =  useState(0);
         useEffect(()=>{
               fetch('./data.json') 
               .then(res=> res.json())
               .then(data=> setAllActors(data))
         },[]);
+         
+
+            const handleSelectActor = actor =>{
+                const isExist = seletedActors.find(item=>item.id==actor.id)
+                let count =  actor.salary;
+                if(isExist){
+                 return alert("All ready book")
+                }else{
+                      seletedActors.forEach(item=>{
+                        count =  count + item.salary
+                      });
+                      console.log(count);
+
+                      const totalRemainng = 20000-count;
+                      setTotalCost (count);
+                      if(totalCost > 20000){
+                       return alert ("taka sesh ar hbe na");
+                      }else{
+                        setReamining(totalRemainng);
+                        setSeletedActors([...seletedActors,actor]);
+                      }
+                 
+                }
               
-          console.log(allActors);
-
+          };
+         
     return (
-          
-
    <>
        <div className="flex">
        
@@ -24,26 +47,37 @@ const Home = () => {
         <div className="ml-9 grid lg:grid-cols-3 gap-10 mt-20">
 
         {
-          allActors.map((actor) => ( <div key={actor.id} className="card w-72 bg-base-100 shadow-xl">
+          allActors.map((actor) => ( <div key={actor.id} className="card w-72  shadow-2xl">
           <figure className="px-10 pt-10">
             <img src={actor.image} alt="Shoes" className="rounded-full w-24 " />
           </figure>
           <div className="card-body items-center text-center">
-            <h2 className="card-title">name:{actor.name}</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, possimus?</p>
-            <div className="flex gap-5">
+            <h2 className="card-title text-yellow-50">name:{actor.name}</h2>
+            <p className='text-yellow-100'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, possimus?</p>
+            <div className="flex gap-5 text-yellow-100">
                <p>salary:{actor.salary}</p> 
                <p>role:{actor.role}</p>
             </div>
             <div className="card-actions">
-            <button className="btn btn-outline btn-secondary">Select</button>
+
+            <button onClick={()=>{
+                 handleSelectActor(actor)
+            }} className="btn btn-outline btn-warning">Select</button>
+
             </div>
           </div>
         </div>) )
         }
 
       </div>
-      <Cart></Cart>
+      <Cart
+         seletedActors={seletedActors}
+         remaining={remaining}
+         totalCost={totalCost}
+    
+      >
+    
+      </Cart>
       </div>
 
      </>
